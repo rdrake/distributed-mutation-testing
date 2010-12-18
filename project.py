47cfe602bca9e0664ccf5bf796c71b1b24359fea
Richard Drake <rdrake@rdrake.org>
2010-12-18 17:59:15 -0500

    Initial import.

---
+++ project.py
@@ -0,0 +1,77 @@
+from os import walk, sep
+from os.path import join, splitext, relpath, pathsep
+
+class Project:
+	def __init__(self, source_path, test_path, mutants=None):
+		self.source_path = source_path
+		self.test_path = test_path
+		self.tests = []
+
+		# Too long to put in the __init__ arguments.
+		if not Mutants:
+			mutants = {
+				"ASK": True,
+				"ASTK": True,
+				"EAN": True,
+				"EELO": True,
+				"ELPA": True,
+				"ESP": True,
+				"EXCR": True,
+				"MBR": True,
+				"MSF": True,
+				"MSP": True,
+				"MXC": True,
+				"MXT": True,
+				"RCXC": True,
+				"RFU": True,
+				"RJS": True,
+				"RNA": True,
+				"RSB": True,
+				"RSK": True,
+				"RSTK": True,
+				"RTXC": True,
+				"RVK": True,
+				"RXO": True,
+				"SHCR": True,
+				"SKCR": True,
+				"SPCR": True
+			}
+	
+	def source_files(self):
+		"""
+		Finds all source files ending in .java.
+		"""
+		for (dirpath, dirnames, filenames) in walk(self.source_path):
+			for filename in filenames:
+				if splitext(filename)[-1] == ".java":
+					print(join(dirpath, filename))
+	
+	def test_names(self):
+		"""
+		Finds all test cases within the specified test path.  It does this by
+		looking for .class files, and massaging the absolute path into the
+		relative java.path.style.
+		"""
+		self.tests = []
+
+		for (dirpath, dirnames, filenames) in walk(self.test_path):
+			for filename in filenames:
+				split = splitext(filename)
+
+				if split[-1] == ".class":
+					abs_path = join(dirpath, split[0])
+
+					if "$" in abs_path: continue
+
+					rel_path = abs_path.replace(self.test_path, "")
+					test_name = rel_path.replace(sep, ".")
+
+					if test_name in self.tests: continue
+
+					yield(test_name)
+
+if __name__ == "__main__":
+	p = Project("/home/rdrake/workspace/junit/src/main/java/", "/home/rdrake/workspace/junit/target/test/java/")
+	
+	for test_name in p.test_names():
+		print(test_name)
---
+++ storage.py
@@ -0,0 +1,27 @@
+from pymongo import Connection
+import gridfs
+
+class FileStore:
+	def __init__(self, db_name):
+		self.db = Connection()[db_name]
+		self.fs = gridfs.GridFS(self.db)
+	
+	def put(self, f, op, fname):
+		"""
+		Places a file f in the storage pool.  Also creates an entry in the
+		database with the operator name used to create the file, and the
+		original file name of the mutant's ancestor.
+		"""
+		return self.fs.put(f, metadata={'op': op, 'file_name': fname, 'kills': 0, 'misses': 0})
+	
+	def get(self, id):
+		"""
+		Retrieves a file given the unique identifier id.
+		"""
+		return self.fs.get(id)
+
+if __name__ == '__main__':
+	f = open("/home/rdrake/.bashrc", 'rb')
+	s = FileStore('dmut')
+	id = s.put(f, 'SUT', 'Account.java')
+	print(s.get(id))
